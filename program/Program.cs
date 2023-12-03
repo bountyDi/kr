f7303343494d772ef3a17e895675082dab565fba
Diana <dina1405@yandex.ru>
2023-12-03 14:35:05 +0600

    program

---
+++ program/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите строки через запятую:");
+string[] Array = Console.ReadLine().Split(',');
+
+//новый массив строк с длиной <= 3 символа
+string[] Array1 = FilterStrings(Array);
+
+Console.WriteLine("Результат:");
+foreach (string str in Array1)
+{
+    Console.Write($"{str} ");
+}
+
+Console.ReadLine();
+
+static string[] FilterStrings(string[] Array)
+{
+    // количество строк с длиной <= 3 символа
+    int count = 0;
+    foreach (string str in Array)
+    {
+        if (str.Length<=3)
+        {
+            count++;
+        }
+    }
+
+//новый массив строк
+string[] Array1 = new string[count];
+
+int index = 0;
+foreach (string str in Array)
+{
+    if (str.Length<=3)
+    {
+        Array1[index] = str;
+        index++;
+    }
+}
+
+return Array1;
+}
